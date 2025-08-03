88c8d415a2983c8972a98f9e6206dfef4dc7dc49
Martin Pham <qdpham88@gmail.com>
2025-08-03 15:01:34 +0200

    Update swagger.blade.php

--- src/resources/views/swagger.blade.php
+++ src/resources/views/swagger.blade.php
@@ -6,17 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content="SwaggerUI" />
     <title>SwaggerUI</title>
-    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.27.1/swagger-ui.css" />
 </head>
 
 <body>
     <div id="swagger-ui"></div>
-    <script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js" crossorigin></script>
+    <script src="https://unpkg.com/swagger-ui-dist@5.27.1/swagger-ui-bundle.js" crossorigin></script>
     <script>
+        function getCookie(name) {
+            const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+            return match ? decodeURIComponent(match[2]) : null;
+        }
+
         window.onload = () => {
             window.ui = SwaggerUIBundle({
                 url: '{{ $json_url }}',
                 dom_id: '#swagger-ui',
+                requestInterceptor: (request) => {
+                    const xsrfToken = getCookie('XSRF-TOKEN');
+                    if (xsrfToken) {
+                        request.headers['X-XSRF-TOKEN'] = xsrfToken;
+                    }
+                    return request;
+                },
             });
         };
     </script>
