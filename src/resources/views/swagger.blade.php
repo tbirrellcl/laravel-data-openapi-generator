d4d194384f5796f478b73c8837bb30898e34f0be
Martin Pham <qdpham88@gmail.com>
2025-08-03 20:35:37 +0200

    Update swagger.blade.php

--- src/resources/views/swagger.blade.php
+++ src/resources/views/swagger.blade.php
@@ -13,22 +13,10 @@
     <div id="swagger-ui"></div>
     <script src="https://unpkg.com/swagger-ui-dist@5.27.1/swagger-ui-bundle.js" crossorigin></script>
     <script>
-        function getCookie(name) {
-            const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-            return match ? decodeURIComponent(match[2]) : null;
-        }
-
         window.onload = () => {
             window.ui = SwaggerUIBundle({
                 url: '{{ $json_url }}',
                 dom_id: '#swagger-ui',
-                requestInterceptor: (request) => {
-                    const xsrfToken = getCookie('XSRF-TOKEN');
-                    if (xsrfToken) {
-                        request.headers['X-XSRF-TOKEN'] = xsrfToken;
-                    }
-                    return request;
-                },
             });
         };
     </script>
