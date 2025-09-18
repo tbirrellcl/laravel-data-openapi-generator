71604f422ccd665131bab4e65ce27b99bce111a7
Timothy Birrell <timothy@closinglock.com>
2025-09-18 12:26:45 -0500

    Mimic error response for fe

--- src/Data/Error.php
+++ src/Data/Error.php
@@ -2,11 +2,54 @@
 
 namespace Xolvio\OpenApiGenerator\Data;
 
+use Illuminate\Http\JsonResponse;
+use ReflectionClass;
 use Spatie\LaravelData\Data;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+use Xolvio\OpenApiGenerator\Attributes\HttpResponseStatus;
 
 class Error extends Data
 {
     public function __construct(
-        public string $message,
-    ) {}
+        public string $error,
+        public ?string $details = null,
+    ) {
+        if ($this->details === null) {
+            $this->details = $this->error;
+        }
+    }
+
+    public function toResponse($request): JsonResponse
+    {
+        return new JsonResponse(
+            array_filter(
+                [
+                    'error'   => $this->error,
+                    'details' => $this->details,
+                ],
+                static fn ($value) => null !== $value,
+            ),
+            $this->resolveStatusCode(),
+        );
+    }
+
+    protected function resolveStatusCode(): int
+    {
+        $reflection = new ReflectionClass(static::class);
+        $attributes = $reflection->getAttributes(HttpResponseStatus::class);
+
+        if (count($attributes) > 0) {
+            $arguments = $attributes[0]->getArguments();
+
+            if (isset($arguments['status'])) {
+                return (int) $arguments['status'];
+            }
+
+            if (isset($arguments[0])) {
+                return (int) $arguments[0];
+            }
+        }
+
+        return HttpResponse::HTTP_INTERNAL_SERVER_ERROR;
+    }
 }
